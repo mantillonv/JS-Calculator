e71a5c99e53662dd708462ed7fb7356f8c740e9b
mantillonv <marceloantillon93@gmail.com>
2023-11-02 18:03:04 -0700

    Html boilerplate, Css and Js template

---
+++ script.js
@@ -0,0 +1,24 @@
+// Elements and Event Listeners //
+
+
+// Basic Math Operators //
+function add {
+
+}
+
+function substract {
+
+}
+
+function multiply {
+
+}
+
+function divide {
+
+}
+
+// Result Display Logic //
+function updateResultDisplay {
+
+}
